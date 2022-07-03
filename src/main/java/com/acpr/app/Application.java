0196b6f468aa61ec100520bc57f55cdd0255b88d
Mingze Ma <mingzema.1998@foxmail.com>
2022-07-03 02:21:51 +0100

    first commit

---
+++ src/main/java/com/acpr/app/Application.java
@@ -0,0 +1,15 @@
+package com.acpr.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
+@EnableOpenApi
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/acpr/app/controller/UserAuthController.java
@@ -0,0 +1,19 @@
+package com.acpr.app.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserAuthController {
+
+    @PostMapping(value = "/login")
+    public String login() {
+        return "Hello World";
+    }
+
+    @PostMapping(value = "/register")
+    public String register() {
+        return "Hello World";
+    }
+}
