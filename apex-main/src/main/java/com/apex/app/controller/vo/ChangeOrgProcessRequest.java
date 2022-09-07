24ccde38e47b96168b585963ed38b48b3e0bd4a5
Mingze Ma <mingzema.1998@foxmail.com>
2022-09-07 03:52:18 +0100

    bug fixed

--- apex-main/src/main/java/com/apex/app/controller/vo/ChangeOrgProcessRequest.java
+++ apex-main/src/main/java/com/apex/app/controller/vo/ChangeOrgProcessRequest.java
@@ -2,6 +2,7 @@ package com.apex.app.controller.vo;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 import java.util.Date;
@@ -10,6 +11,7 @@ import java.util.Date;
  * @author Mingze Ma
  */
 @Data
+@NoArgsConstructor
 public class ChangeOrgProcessRequest {
 
     @NotEmpty
